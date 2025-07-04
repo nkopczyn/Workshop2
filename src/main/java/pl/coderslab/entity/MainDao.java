4403c0c9931cb0b13ce1baafbba765543c3ffc18
“nkopczyn” <nkopczyn@visa.com>
2025-07-04 11:02:59 +0200

    read method

--- src/main/java/pl/coderslab/entity/MainDao.java
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -3,12 +3,20 @@ package pl.coderslab.entity;
 public class MainDao {
     public static void main(String[] args) {
 
-        User user = new User();
-        user.setUserName("basia");
-        user.setEmail("basia@mail.com");
-        user.setPassword("basia1234");
+          User user = new User();
+//        user.setUserName("janek");
+//        user.setEmail("janek@mail.com");
+//        user.setPassword("janek1234");
 
-        UserDao.createUser(user);
+        //UserDao.createUser(user);
+
+        //System.out.println(user.getUserName());
+
+        User readUserResult = UserDao.readUser(4);
+        System.out.println(readUserResult.getUserName());
+        System.out.println(readUserResult.getPassword());
+
+
 
     }
 }
--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -1,26 +1,76 @@
 package pl.coderslab.entity;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class UserDao {
 
     private static final String CREATE_QUERY = "INSERT INTO workshop2 (email, username, password) VALUES (?, ?, ?)";
+    private static final String CHECK_ID = "SELECT * FROM workshop2 WHERE id = ?";
 
-    public static void createUser(User user) {
 
-        try {
-            Connection connection = DbUtil.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(CREATE_QUERY);
-            preparedStatement.setString(1, user.getUserName());
-            preparedStatement.setString(2, user.getEmail());
-            preparedStatement.setString(3, user.getPassword());
-            preparedStatement.executeUpdate();
+    // creating new user
+    public static User createUser(User user) {
 
-        } catch (Exception e) {
+        try
+                (Connection connection = DbUtil.getConnection()) {
+
+            PreparedStatement preparedStatement1 = connection.prepareStatement(CREATE_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement1.setString(1, user.getEmail());
+            preparedStatement1.setString(2, user.getUserName());
+            preparedStatement1.setString(3, user.getPassword());
+            preparedStatement1.executeUpdate();
+
+            ResultSet resultSet1 = preparedStatement1.getGeneratedKeys();
+            if (resultSet1.next()) {
+                user.setId(resultSet1.getInt(1));
+            }
+            return user;
+
+        } catch (SQLException e) {
             e.printStackTrace();
+            return null;
         }
 
+    }
 
+
+    // reading existing user form database, taking in a row from table and turning it into na object
+    public static User readUser(int idUser) {
+
+        // check connection
+        try
+                (Connection connection = DbUtil.getConnection();
+                 PreparedStatement preparedStatement2 = connection.prepareStatement(CHECK_ID)) {
+
+            preparedStatement2.setInt(1, idUser); // setting value for prepared statement
+
+            try (ResultSet resultSet2 = preparedStatement2.executeQuery()) {
+                if (resultSet2.next()) {
+                    // if id is in the database, checks if the sql query returned at least one row
+                    // there is a row with the given id
+
+                    System.out.println("ID found in database");
+
+                    // retrieve info
+                    User userResult = new User();
+                    userResult.setId(resultSet2.getInt("id"));
+                    userResult.setEmail(resultSet2.getString("email"));
+                    userResult.setUserName(resultSet2.getString("username"));
+                    userResult.setPassword(resultSet2.getString("password"));
+
+                    // print info, need to call getter to see user's info
+                    return userResult;
+
+                } else {
+                    System.out.println("ID not found in database");
+                    return null;
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
